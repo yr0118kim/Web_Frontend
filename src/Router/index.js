b10d82caa30a77ad7df544c25eeba262aa680d22
kimyerim01118 <72791856+yr0118kim@users.noreply.github.com>
2023-01-15 01:03:24 +0900

    11

--- src/Router/index.js
+++ src/Router/index.js
@@ -1,14 +1,22 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage";
+<<<<<<< Updated upstream
 import SignupPage from "../pages/SignupPage";
 
+=======
+import LoginPage from "../pages/LoginPage";
+>>>>>>> Stashed changes
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage/>} />
+<<<<<<< Updated upstream
         <Route path="/signup" element={<SignupPage/>} />
+=======
+        <Route path="/Login" element={<LoginPage/>} />
+>>>>>>> Stashed changes
       </Routes>
     </BrowserRouter>
   );
